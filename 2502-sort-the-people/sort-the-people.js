536c61b08a9d5b2f6c533e88097bd4d2b3f2e7c7
Hudhyfa <121671156+hudhyfa@users.noreply.github.com>
2024-05-19 22:58:33 +0530

    Time: 77 ms (48.34%) | Memory: 54.3 MB (68.64%) - LeetSync

--- 2502-sort-the-people/sort-the-people.js
+++ 2502-sort-the-people/sort-the-people.js
@@ -3,14 +3,15 @@
  * @param {number[]} heights
  * @return {string[]}
  */
-var sortPeople = function (names, heights) {
+var sortPeople = function(names, heights) {
     let namesMap = new Map();
-    for (let i = 0; i < names.length; i++) {
+    let result = [];
+    for(let i = 0; i < names.length; i++) {
         namesMap.set(heights[i], names[i]);
     }
-    let sortedHeights = heights.sort((a, b) => b - a);
-    for (let i = 0; i < sortedHeights.length; i++) {
-        names[i] = namesMap.get(sortedHeights[i]);
+    let sortedHeights = heights.sort((a,b) => b - a);
+    for(let i = 0; i < sortedHeights.length; i++) {
+        result.push(namesMap.get(sortedHeights[i]))
     }
-    return names;
+    return result;
 };
