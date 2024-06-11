23a467bb86fe43594fcf084c4eb584f0807589c5
Hudhyfa <121671156+hudhyfa@users.noreply.github.com>
2024-06-11 07:22:30 +0530

    Time: 50 ms (94.92%), Space: 53 MB (20.34%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.ts
@@ -0,0 +1,25 @@
+function relativeSortArray(arr1: number[], arr2: number[]): number[] {
+    let hashMap: { [key: number]: number } = {};
+    let result: number[] = []; 
+    let sortedHalf: number[] = [];
+    for (let idx in arr1) {
+        hashMap[arr1[idx]] = (hashMap[arr1[idx]] || 0) + 1;
+    }
+    for (let idx in arr2) {
+        while (hashMap[arr2[idx]] > 0) {
+            result.push(arr2[idx]);
+            hashMap[arr2[idx]] -= 1;
+        }
+    }
+    for (let idx in arr1) {
+        if (hashMap[arr1[idx]] > 0) {
+            while (hashMap[arr1[idx]] > 0) {
+                sortedHalf.push(arr1[idx]);
+                hashMap[arr1[idx]] -= 1;
+            }
+        }
+    }
+    sortedHalf.sort((a, b) => a - b);
+    return [...result, ...sortedHalf];
+
+};
