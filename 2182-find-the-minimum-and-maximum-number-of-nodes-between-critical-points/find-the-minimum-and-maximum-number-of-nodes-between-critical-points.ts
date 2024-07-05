3ef021aaa545af2639e61d9146a5043141121c4e
Hudhyfa <121671156+hudhyfa@users.noreply.github.com>
2024-07-05 09:35:25 +0530

    Time: 140 ms (88.89%) | Memory: 83.5 MB (11.11%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.ts
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.ts
@@ -13,24 +13,21 @@
 function nodesBetweenCriticalPoints(head: ListNode | null): number[] {
     let minDistance: number | null = null;
 
+    //to track and store the position of critical nodes.
     let firstCriticalNode: number | null = null;
     let lastCriticalNode: number | null = null;
     let followerOfLastCriticalNode: number | null = null;
 
-    let prevVal: number | null = null;
-    let cursor: ListNode = head;
-    let nodeCount: number = 0;
-    let result: number[] = [];
+    let prevVal: number | null = null; // maintains the previous node's value. 
+    let cursor: ListNode = head;       // travel the whole linked-list.
+    let nodeCount: number = 0;         // will maintain the count of nodes.
 
     while (cursor) {
-
         nodeCount += 1;
-
         if (prevVal && cursor.next) {
-
             if ((cursor.val > prevVal && cursor.val > cursor.next.val) ||
-                (cursor.val < prevVal && cursor.val < cursor.next.val)
-            ) {
+                (cursor.val < prevVal && cursor.val < cursor.next.val)) {
+
                 if (!firstCriticalNode) {
                     firstCriticalNode = nodeCount;
                     lastCriticalNode = nodeCount;
@@ -40,7 +37,6 @@ function nodesBetweenCriticalPoints(head: ListNode | null): number[] {
                     lastCriticalNode = nodeCount;
                 }
 
-
                 if (lastCriticalNode !== firstCriticalNode) {
                     if (!minDistance) {
                         minDistance = lastCriticalNode - firstCriticalNode;
@@ -49,21 +45,18 @@ function nodesBetweenCriticalPoints(head: ListNode | null): number[] {
                             minDistance = lastCriticalNode - followerOfLastCriticalNode;
                         }
                     }
-
                 }
             }
         }
         prevVal = cursor.val;
         cursor = cursor.next;
     }
-    if(nodeCount < 4) return [-1, -1];
+    if (nodeCount < 4) return [-1, -1];
 
-    if(firstCriticalNode && lastCriticalNode) {
-        if(firstCriticalNode !== lastCriticalNode) {
-            result.push(minDistance);
-            result.push(lastCriticalNode - firstCriticalNode);
-        }
+    if (firstCriticalNode && lastCriticalNode) {
+        if (firstCriticalNode !== lastCriticalNode) 
+            return [minDistance, lastCriticalNode - firstCriticalNode]
     }
 
-    return result.length > 0 ? result: [-1, -1];
+    return [-1, -1];
 };
