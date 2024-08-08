31528e66e4bf434d910197a37a7cefc853e2bf78
Hudhyfa <121671156+hudhyfa@users.noreply.github.com>
2024-08-08 11:16:57 +0530

    Time: 68 ms (67.77%), Space: 54 MB (51.65%) - LeetHub

--- 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.js
+++ 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.js
@@ -13,10 +13,8 @@ var countWords = function(words1, words2) {
     for(let str of words2) {
         obj2[str] = (obj2[str] || 0) + 1;
     }
-    for(let str of words2) {
-        if(obj1[str] && obj2[str]) {
-            if(obj1[str] === 1 && obj2[str] === 1) ++result;
-        }
+    for(str in obj2) {
+        if(obj1[str] === 1 && obj2[str] === 1) ++result;
     }
     return result;
 };
